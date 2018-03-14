97abb53dc02448bcf18469b1b5261e9f4942e68e
porzingod <jasonlim.ny@gmail.com>
2018-03-14 16:55:06 -0400

    Done.

--- crewMember.js
+++ crewMember.js
@@ -1,1 +1,42 @@
-class CrewMember {}
+// let CrewMember = (function() {
+//   let id = 0
+//   let all = []
+
+class CrewMember {
+  constructor(position) {
+    this.position = position
+    this.currentShip = "Looking for a Rig"
+    // this.id = ++id
+
+    // all.push(this)
+  }
+
+  // static all() {
+  //   return [...all]
+  // }
+
+  engageWarpDrive() {
+    if (this.position !== "Pilot" || this.currentShip === "Looking for a Rig") {
+      return "had no effect"
+    } else {
+      this.currentShip.warpDrive = "engaged"
+    }
+  }
+
+  setsInvisibility() {
+    if (this.position !== "Defender" || this.currentShip === "Looking for a Rig") {
+      return "had no effect"
+    } else {
+      this.currentShip.cloaked = true
+    }
+  }
+
+  chargePhasers() {
+    if (this.position !== "Gunner" || this.currentShip === "Looking for a Rig") {
+      return "had no effect"
+    } else {
+      this.currentShip.phasersCharge = "charged"
+    }
+  }
+}
+// })()
--- spaceship.js
+++ spaceship.js
@@ -1,1 +1,24 @@
-class Spaceship {}
+class Spaceship {
+    constructor(name, crew, phasers, shields) {
+      this.name = name
+      this.crew = crew
+      // if (crew === undefined) {
+      //   this.crew = []
+      // } else if (Array.isArray(crew)) {
+      //   this.crew = crew
+      // } else {
+      //   this.crew = [crew]
+      // }
+      this.phasers = phasers
+      this.shields = shields
+      this.cloaked = false
+      // this.docked = true
+      this.crew.length === 0 ? this.docked = true : this.docked = false
+      this.warpDrive = "disengaged"
+      this.phasersCharge = "uncharged"
+
+      for(let member of crew) {
+        member.currentShip = this
+      }
+    }
+}
