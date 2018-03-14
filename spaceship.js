f2be5c0831126fe691ac94612576b10336628556
cs-j <cj@mail.co.de>
2018-03-14 16:42:05 -0400

    spaceShip.js tests

--- spaceship.js
+++ spaceship.js
@@ -1,1 +1,16 @@
-class Spaceship {}
+class Spaceship {
+  constructor (name, crewMembers, phasers, shields) {
+    this.name = name
+    this.crewMembers = crewMembers
+    this.phasers = phasers
+    this.shields = shields
+    this.cloaked = false
+    this.warpDrive = 'disengaged'
+    this.docked = this.isDocked()
+    this.phasersCharge = 'uncharged'
+  }
+
+  isDocked () {
+    return this.crewMembers.length === 0 ? true : false
+  }
+}
