2037c8d03d8093dac862fc80b5cd4aed58216fdc
zitro <bryan@bryanortiz.com>
2018-03-15 07:27:55 -0400

    Done.

--- crewMember.js
+++ crewMember.js
@@ -1,1 +1,24 @@
-class CrewMember {}
+class CrewMember {
+  constructor(position, currentShip = 'Looking for a Rig') {
+    this.position = position
+    this.currentShip = currentShip
+  }
+  engageWarpDrive() {
+    if (this.currentShip === 'Looking for a Rig'){
+      return 'had no effect'
+    }
+  }
+  setsInvisibility() {
+    if (this.currentShip === 'Looking for a Rig'){
+      return 'had no effect'
+    } else if (this.position === 'Defender') {
+      this.currentShip.cloaked = true
+      return this.currentShip.cloaked
+    }
+  }
+  chargePhasers() {
+    if (this.currentShip === 'Looking for a Rig'){
+      return 'had no effect'
+    }
+  }
+}
